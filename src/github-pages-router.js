9c1864424676d6b5baeb3b4ff483f9170bf82283
dleeftink <dennisleeftink@gmail.com>
2024-11-29 21:01:46 +1000

    Revert to pre-debugging router

--- src/github-pages-router.js
+++ src/github-pages-router.js
@@ -1,78 +1,71 @@
+/*! fibo.github.io/github-pages-router • MIT License */
 (function GitHubPagesRouter() {
-
   function defineComponent(elementName, ElementClass) {
-    if (!customElements.get(elementName))
-      customElements.define(elementName, ElementClass);
+    if (customElements.get(elementName)) return
+    customElements.define(elementName, ElementClass)
   }
 
-  let main = document.getElementsByTagName('main');
-
-  window.addEventListener('pageswap', async (event) => {
-
-    sessionStorage.setItem('lastVisit', main[0].innerHTML);
-
-  });
-
+  /**
+   * Web component <ghp-router>. All other ghp-* components must be inside a <ghp-router>.
+   */
   class GHPRouter extends HTMLElement {
-    contentElement = void 0;
-
-    navlinks = new Set();
-    contentMap = new Map();
-    routes = [];
-
-    debug = window.debug ?? false;
-
-    constructor() {
-      super();
-      window.GHPRouter = this;
-    }
+    /** DOM Element that wraps the content, defaults to <main> tag. */
+    contentElement = undefined
+    navlinks = new Set
+    contentMap = new Map
+    routes = []
 
     connectedCallback() {
-      addEventListener("popstate", this);
-      this.contentElement = document.querySelector(
-        this.getAttribute("outlet") ?? "main",
-      );
-      if (!this.contentElement) console.error("Cannot find contentElement");
+      addEventListener("popstate", this)
+      this.contentElement = document.querySelector(this.getAttribute("outlet") ?? "main")
+      if (!this.contentElement) console.error("Cannot find contentElement")
+
+      // convenience listener to store last page content (not sure if needed)
+
+      // let main = document.getElementsByTagName('main')[0];
+      // window.addEventListener('pageswap', async (event) => {
+      //   sessionStorage.setItem('lastVisit', main.innerHTML);
+      // });
+
     }
 
-    async handleEvent(event) {
+    handleEvent(event) {
       if (event.type == "popstate") {
-        const contentUrl = this.contentUrlFromLocation(location.toString());
-        if (contentUrl) await this.viewTransition(contentUrl);
+        const contentUrl = this.contentUrlFromLocation(location.toString())
+        if (contentUrl) this.viewTransition(contentUrl)
       }
     }
 
     contentUrlFromLocation(url) {
-      const matchedRoute = this.routes.find(
-        ({ href }) => url == new URL(href, document.baseURI),
-      );
-      if (matchedRoute) {
-        let contentUrl = new URL(matchedRoute.content, document.baseURI).toString();
-        return contentUrl
-      }
+      const matchedRoute = this.routes.find(({ href }) => url == new URL(href, document.baseURI))
+      if (!matchedRoute) return
+      return new URL(matchedRoute.content, document.baseURI).toString()
     }
 
-    async navigate(event) {
-      event.preventDefault();
-      const { href } = event.target;
-      if (href == document.location.toString()) return;
-      const contentUrl = this.contentUrlFromLocation(href);
-      if (!contentUrl) return;
-      history.pushState({}, "", href);
-      await this.viewTransition(contentUrl);
+    /**
+     * Handle anchor click event.
+     */
+    navigate(event) {
+      event.preventDefault()
+      const { href } = event.target
+      if (href == document.location.toString()) return
+      const contentUrl = this.contentUrlFromLocation(href)
+      if (!contentUrl) return
+      history.pushState({}, "", href)
+      this.viewTransition(contentUrl)
     }
 
     async viewTransition(contentUrl) {
       if (!document.startViewTransition) return await this.updateContent(contentUrl);
-      let last = sessionStorage.getItem('lastVisit')
-      if(this.debug) console.log('Setting', last);
-      this.contentElement.innerHTML = last;
+
+      // convenience setter to ensure main content is what has been loaded last (not sure if needed)
+      // let last = sessionStorage.getItem('lastVisit')
+      // this.contentElement.innerHTML = last; 
+
       const transition = document.startViewTransition(async () => {
         await this.updateContent(contentUrl);
-      });
-
+      })
       await transition.finished;
-
     }
 
     async updateContent(url) {
@@ -81,22 +74,18 @@
 
       return new Promise(async (keep, drop) => {
         try {
-          if (sessionStorage.getItem(url)) {
-            //setTimeout(() => {
-              contentElement.innerHTML = //this.contentMap.get(url);
-                sessionStorage.getItem(url);
-                keep()
-             //}, /* notice, no timeout here */)
+          if (sessionStorage.getItem(url) /*this.contentMap.has(url)*/) {
+            contentElement.innerHTML = // this.contentMap.get(url); 
+              sessionStorage.getItem(url);
+            keep()
           } else {
             const response = await fetch(url);
             const text = await response.text();
-            //this.contentMap.set(url, text);
-            sessionStorage.setItem(url, text);
+            sessionStorage.setItem(url, text); // this.contentMap.set(url, text);
             contentElement.innerHTML = text;
-            //localStorage.setItem('contentMap', JSON.stringify(Array.from(this.contentMap.entries())));
             keep()
           }
-          for (const navlink of this.navlinks.values()) navlink.setAriaCurrent();
+          for (const navlink of this.navlinks.values()) navlink.setAriaCurrent(); // does this need to executed before promise is revolsed?
         } catch (error) {
           console.error(error);
           drop(error);
@@ -105,146 +94,118 @@
     }
   }
 
-  defineComponent("ghp-router", GHPRouter);
-
-  /* rewrite as contextRequestPattern */
+  defineComponent("ghp-router", GHPRouter)
 
   function findParentRouter(initialElement) {
-    /*let { parentElement: element } = initialElement;
+    let { parentElement: element } = initialElement
     while (element) {
-      if (element.localName == "ghp-router") return element;
+      if (element.localName == "ghp-router") return element
       element = element.parentElement;
     }
-    throw new Error(`No ghp-router found for element ${initialElement}`);*/
-    return window.GHPRouter;
+    console.error(`No ghp-router found for element ${initialElement}`)
   }
 
+  /**
+   * Web component <ghp-route>.
+   *
+   * It requires the following attributes:
+   * - route
+   * - content: URL to HTML content file.
+   *
+   * @example
+   * ```html
+   * <ghp-route route="./" content="./path/to/file.html"></ghp-route>
+   * ```
+   */
   class GHPRoute extends HTMLElement {
-    router = void 0;
-
-    async connectedCallback() {
-      try {
-        this.router = findParentRouter(this);
-      } catch (error) {
-        console.error(error);
-        return;
-      }
-      const href = this.getAttribute("href");
-      const content = this.getAttribute("content");
-      if (!href || !content) {
-        console.error("Missing href or content attribute");
-        return;
-      }
-
-      this.router.routes.push({ href, content });
-
-      if (new URL(href, document.baseURI).toString() == location.toString()) {
-        //window.addEventListener("load", async () => {
-          if(this.router.debug) console.log('Called viewTransition from route')
-          await this.router.viewTransition(
-            new URL(content, document.baseURI).toString(),
-          );
-       // })
-      }
-
-      let contentUrl = new URL(content, document.baseURI).toString()
-      this.backgroundFetch(contentUrl);
-
-    }
-
-    /* not really background fetch-> needs worker */
-
-    backgroundFetch(contentUrl) {
-      // if not cached and not on root
-      if(!sessionStorage.getItem(contentUrl) && location.pathname !== '/') {
-      fetch(contentUrl)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          return response.text();
-        })
-        .then(data => {
-          // Store the fetched data
-          sessionStorage.setItem(contentUrl, data)
-          if(this.router.debug) console.log('Prefetched content:', contentUrl, data);
-        })
-        .catch(error => {
-          if(this.router.debug) console.error('Fetch error:', error);
-        });
-      }
-    }
-  }
-
-  defineComponent("ghp-route", GHPRoute);
-
-  class GHPLink extends HTMLElement {
-
-    router = void 0;
+    router = undefined
 
     connectedCallback() {
-      try {
-        this.router = findParentRouter(this);
-      } catch (error) {
-        console.error(error);
+      this.router = findParentRouter(this)
+      if (!this.router) return
+      const href = this.getAttribute("href")
+      const content = this.getAttribute("content")
+      if (!href || !content) {
+        console.error("Missing href or content attribute")
+        return
       }
-      this.anchor?.addEventListener("click", this);
+      this.router.routes.push({ href, content })
+      if (new URL(href, document.baseURI).toString() == location.toString()) {
+        this.router.viewTransition(new URL(content, document.baseURI).toString())
+      }
+    }
+  }
+
+  defineComponent("ghp-route", GHPRoute)
+
+  /**
+   * Web component <ghp-link> handles an anchor that points to a route.
+   * It must wrap the anchor, and will override its click event.
+   * @example
+   * ```html
+   * <ghp-link><a href="./some-route">Click me</a></ghp-link>
+   * ```
+   */
+  class GHPLink extends HTMLElement {
+    router = undefined
+
+    connectedCallback() {
+      this.router = findParentRouter(this)
+      if (!this.router) return
+      this.anchor?.addEventListener("click", this)
     }
 
     get anchor() {
-      return this.querySelector("a");
+      return this.querySelector("a")
     }
 
-    async handleEvent(event) {
-      if (event.type == "click" && event.target == this.anchor)
-        await this.router?.navigate(event);
+    handleEvent(event) {
+      if (event.type == "click" && event.target == this.anchor) {
+        this.router?.navigate(event)
+      }
     }
   }
 
-  defineComponent("ghp-link", GHPLink);
+  defineComponent("ghp-link", GHPLink)
 
+  /**
+   * Web component <ghp-navlink> is similar to <ghp-link> but it also adds aria-selected="page" if the anchor points to current location.
+   */
   class GHPNavlink extends HTMLElement {
-    router = void 0;
+    router = undefined
 
     connectedCallback() {
-      try {
-        this.router = findParentRouter(this);
-      } catch (error) {
-        console.error(error);
-      }
-      this.anchor?.addEventListener("click", this);
-      this.setAriaCurrent();
-      this.router?.navlinks.add(this);
-      if(new URL(this.anchor?.href, document.baseURI).toString() == location.toString()) {
-        this.classList.add('active');
-        this.anchor.focus();
-      }
+      this.router = findParentRouter(this)
+      if (!this.router) return
+      this.anchor?.addEventListener("click", this)
+      this.setAriaCurrent()
+      this.router.navlinks.add(this)
     }
 
     disconnectedCallback() {
-      this.router?.navlinks.delete(this);
+      this.router?.navlinks.delete(this)
     }
 
     get anchor() {
-      return this.querySelector("a");
+      return this.querySelector("a")
     }
 
-    async handleEvent(event) {
-      if (event.type == "click" && event.target == this.anchor)
-        await this.router?.navigate(event);
+    handleEvent(event) {
+      if (event.type == "click" && event.target == this.anchor) {
+        this.router?.navigate(event)
+      }
     }
 
     setAriaCurrent() {
       const { anchor } = this;
       if (!anchor) return;
       if (anchor.href == document.location.toString()) {
-        anchor.setAttribute("aria-current", "page");
+        anchor.setAttribute("aria-current", "page")
       } else {
-        anchor.setAttribute("aria-current", "");
+        anchor.setAttribute("aria-current", "")
       }
     }
   }
 
-  defineComponent("ghp-navlink", GHPNavlink);
-
+  defineComponent("ghp-navlink", GHPNavlink)
 })();
